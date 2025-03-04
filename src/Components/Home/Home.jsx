3ccddf6ae8179bb7c4e625f69ca27ccf1fce84f2
jnakht <jisan.cse.cu@gmail.com>
2025-03-04 14:29:33 +0600

    loader or spinner using useNavigation

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,14 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 
 const Home = () => {
+    const navigation = useNavigation();
     return (
         <div>
             <Header></Header>
             <h2>This is home component</h2>
-            <Outlet></Outlet>
+            {
+                navigation.state === 'loading' ?
+                <p style={{fontSize: '40px'}}>Loading...</p> :
+                <Outlet></Outlet>
+            }
+            
             <Footer></Footer>
         </div>
     );
