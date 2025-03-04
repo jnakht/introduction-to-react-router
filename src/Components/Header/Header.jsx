49e02ecd92959795ceb6c630ca9f50895229ce80
jnakht <jisan.cse.cu@gmail.com>
2025-03-04 14:17:22 +0600

    active route

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,5 @@
 
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css'
 const Header = () => {
     return (
@@ -7,11 +7,13 @@ const Header = () => {
             <h2>Navbar</h2>
             <ul className="">
                 
-                <Link to="/">Home</Link>
-                <Link to="/users">Users</Link>
-                <Link to="/posts">Posts</Link>
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact Us</Link>
+                
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/users'>Users</NavLink>
+                <NavLink to='/posts'>Posts</NavLink>
+                <NavLink to='/about'>About</NavLink>
+                <NavLink to='/contact'>Contact Us</NavLink>
+                
                 
                
             </ul>
